464d37bbfe5db6284d5761b1f266f8f93b51f676
Karim Afettouche <krymo86@yahoo.com>
2022-09-01 13:56:02 +0200

    final try

---
+++ main.js
@@ -0,0 +1,26 @@
+// Selectors
+const pageWrapper = document.querySelector(".page-header");
+
+// Create secondary navigation bar
+const htmlElement = document.createElement("div");
+htmlElement.classList.add("secondary-navigation");
+htmlElement.innerHTML = `
+    <div class = "navi__item">
+    <img src="./images/spinner11 (1).svg" alt="time reload">
+    <span> 30 Tage Gratis Rückversand </span>
+    </div>
+
+    <div class = "navi__item">
+    <img src="./images/Icon awesome-heart (1).svg" alt="time reload">
+    <span> Nachhaltige Produkte </span>
+    </div>
+
+    <div class = "navi__item">
+    <img src="./images/Trusted_Shops-Trustmark.png" alt="time reload">
+    <span> 4.87/5 </span>
+    </div>
+  
+   `;
+document.body.appendChild(htmlElement);
+pageWrapper.appendChild(htmlElement);
+console.log(pageWrapper);
